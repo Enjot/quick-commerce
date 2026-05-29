938568db44f98a192086cba6d02e4cbf5bdce251
Jakub Nahacz <jakubnahacz@gmail.com>
2026-05-29 19:30:00 +0200

    Initial commit

---
+++ src/main/java/com/enjot/quickcommerce/QuickCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.enjot.quickcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCommerceApplication.class, args);
+	}
+
+}
