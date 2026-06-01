1b6e83b34c046e7039b0d0f072ea89b021022619
Jakub Nahacz <jakubnahacz@gmail.com>
2026-06-01 18:31:12 +0200

    feat: add Category entity and repository

---
+++ src/main/java/com/enjot/quickcommerce/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.enjot.quickcommerce.repository;
+
+import com.enjot.quickcommerce.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
+}
